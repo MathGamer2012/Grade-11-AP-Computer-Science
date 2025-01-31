ef7a34f08b7213660d4fdb540905b6bf70471226
Ishaan Mittal <123089344+MathGamer2012@users.noreply.github.com>
2025-01-31 13:20:17 -0500

    Add files via upload

---
+++ Labs/Lab3/CalcAge/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Name: Ishaan Mittal 
+//Date: <Sept 19 - Sept 19>
+//Assignment: Inputs 
+//Purpose: To calculate age 
+
+namespace CalcAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days;
+            int hours;
+            int min;
+            int sec;
+            int value;
+            string input;
+
+
+            Console.Write("Enter the number of seconds: ");
+            input = Console.ReadLine();
+            value = int.Parse(input);
+
+            days = value / 86400;
+            value = value - days * 86400;
+
+            Console.WriteLine("Days: " + days);
+
+            hours = value / 3600;
+            value = value - hours * 3600;
+
+            Console.WriteLine("Hours: " + hours);
+
+            min = value / 60;
+            value = value - min * 60;
+
+            Console.WriteLine("Minutes: " + min);
+
+            sec = value; 
+
+            Console.WriteLine("Seconds: " + sec); 
+
+          
+            Console.ReadKey(); 
+
+        }
+    }
+}
---
+++ Labs/Lab3/Inputs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Name: Ishaan Mittal 
+//Date: <Sept 19 - Sept 19>
+//Assignment: Inputs 
+//Purpose: To make inputs 
+
+namespace Inputs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number: ");
+            string input = Console.ReadLine();
+            int value = int.Parse(input);
+            value = value * 3;
+            Console.WriteLine(input + " x 3 = " + value);
+            Console.ReadKey(); 
+        }
+    }
+}
