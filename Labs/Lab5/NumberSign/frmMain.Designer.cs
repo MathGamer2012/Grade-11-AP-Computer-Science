17ec3a9ef5c2ac50e454d764aef4c3f7334b3878
Ishaan Mittal <123089344+MathGamer2012@users.noreply.github.com>
2025-01-31 13:28:32 -0500

    Add files via upload

---
+++ Labs/Lab5/NumberSign/frmMain.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace NumberSign
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.btnNumCheck = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(45, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number 1:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(45, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Number 2:";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(146, 40);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 20);
+            this.txtNum1.TabIndex = 3;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(146, 70);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 20);
+            this.txtNum2.TabIndex = 4;
+            // 
+            // btnNumCheck
+            // 
+            this.btnNumCheck.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnNumCheck.Location = new System.Drawing.Point(103, 111);
+            this.btnNumCheck.Name = "btnNumCheck";
+            this.btnNumCheck.Size = new System.Drawing.Size(80, 23);
+            this.btnNumCheck.TabIndex = 5;
+            this.btnNumCheck.Text = "Num Check";
+            this.btnNumCheck.UseVisualStyleBackColor = false;
+            this.btnNumCheck.Click += new System.EventHandler(this.btnNumCheck_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 170);
+            this.Controls.Add(this.btnNumCheck);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "Number Sign";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Button btnNumCheck;
+    }
+}
+
---
+++ Labs/Lab5/NumberSign/frmMain.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Name: Ishaan Mittal 
+//Date: <Sept 29 - Sept 29>
+//Assigment: NumberSign 
+//Purpose: To check is num1 and num2 are postive or negative numbers 
+
+namespace NumberSign
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNumCheck_Click(object sender, EventArgs e)
+        {
+            double num1 = double.Parse(txtNum1.Text);
+            double num2 = double.Parse(txtNum2.Text);
+
+            if (num1 < 0 && num2 < 0)
+            {
+                MessageBox.Show("Both are negative!");
+            }
+
+            else if (num1 < 0)
+            {
+                MessageBox.Show("Number 1 is a negative!");
+            }
+
+            else if (num2 < 0)
+            {
+                MessageBox.Show("Number 2 is a negative!");
+            }
+
+            if (num1 >= 0 && num2 >= 0) {
+
+                MessageBox.Show("Both are positive"); 
+            }
+
+            else if (num1 >= 0)
+            {
+                MessageBox.Show("Number 1 is a positive!");
+            }
+
+            else if (num2 >= 0)
+            {
+                MessageBox.Show("Number 2 is a positive!");
+            } 
+
+            MessageBox.Show("Done");
+        }
+    }
+}
